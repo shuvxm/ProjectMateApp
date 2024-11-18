64a43e8704559f2f92c6a97d77646c169502f7b8
shuvxm <kumarshubham5559@gmail.com>
2024-11-18 21:38:50 +0530

    "Project Mate Application-backend"

--- PMA/src/main/java/com/shuvxm/PMA/repository/ProjectRepo.java
+++ PMA/src/main/java/com/shuvxm/PMA/repository/ProjectRepo.java
@@ -2,14 +2,16 @@ package com.shuvxm.PMA.repository;
 
 import com.shuvxm.PMA.entity.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface ProjectRepo extends JpaRepository<Project,Long> {
+public interface ProjectRepo extends JpaRepository<Project, Long> {
 
-
+    @Query("SELECT p FROM Project p WHERE p.owner.id = :ownerId")
     List<Project> findProjectsByOwner(Long ownerId);
 
+    @Query("SELECT DISTINCT p FROM Project p JOIN p.team t WHERE t.id = :userId")
     List<Project> findProjectsByTeamMember(Long userId);
+
 }
