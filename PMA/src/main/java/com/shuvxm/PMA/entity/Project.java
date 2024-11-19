0a9366a92650b21b7b5d9041dcb02be8c1d5adf1
shuvxm <kumarshubham5559@gmail.com>
2024-11-19 10:14:13 +0530

    "Project Mate Application-backend"

--- PMA/src/main/java/com/shuvxm/PMA/entity/Project.java
+++ PMA/src/main/java/com/shuvxm/PMA/entity/Project.java
@@ -22,11 +22,16 @@ public class Project {
     private String name;
     private String description;
     private String category;
+    //    @ElementCollection
     private List<String> tags = new ArrayList<>();
 
-    @JsonIgnore
-    @OneToOne(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)  // this mappedBy will go to chat and mapped with chat project
+    //    @JsonIgnore
+//    @OneToOne(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)  // this mappedBy will go to chat and mapped with chat project
+//    private Chat chat;
+    @OneToOne
+    @JoinColumn(name = "chat_id")
     private Chat chat;
+
 
     @ManyToOne
     private User owner;
@@ -34,6 +39,14 @@ public class Project {
     @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Issue> issues = new ArrayList<>();
 
+    //    @ManyToMany
+//    private List<User> team = new ArrayList<>();
     @ManyToMany
+    @JoinTable(
+            name = "user_project_team",
+            joinColumns = @JoinColumn(name = "project_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
     private List<User> team = new ArrayList<>();
+
 }
