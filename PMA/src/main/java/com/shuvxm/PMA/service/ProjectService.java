f5931d73a17e4af597479bbb4372e7c4b22bc0a3
shuvxm <kumarshubham5559@gmail.com>
2024-11-19 10:14:41 +0530

    "Project Mate Application-backend"

--- PMA/src/main/java/com/shuvxm/PMA/service/ProjectService.java
+++ PMA/src/main/java/com/shuvxm/PMA/service/ProjectService.java
@@ -6,4 +6,6 @@ import org.springframework.http.ResponseEntity;
 public interface ProjectService {
 
     ResponseEntity<?> createProject(Project project);
+
+    ResponseEntity<?> findAllProjects();
 }
