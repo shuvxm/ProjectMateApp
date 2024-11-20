fecb854b870af75c4e72cc91a0b33114a8ea610c
shuvxm <kumarshubham5559@gmail.com>
2024-11-20 09:51:14 +0530

    "Project Mate Application-backend"

--- PMA/src/main/java/com/shuvxm/PMA/controller/ProjectController.java
+++ PMA/src/main/java/com/shuvxm/PMA/controller/ProjectController.java
@@ -1,4 +1,36 @@
 package com.shuvxm.PMA.controller;
 
+import com.shuvxm.PMA.entity.Project;
+import com.shuvxm.PMA.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("projects")
 public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @PostMapping
+    public ResponseEntity<?> createProject(@RequestBody Project project){
+        return projectService.createProject(project);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> findAllProjects(){
+        return projectService.findAllProjects();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> findProjectById(@PathVariable Long id){
+        return projectService.findProjectById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProjectById(@PathVariable Long id){
+        return projectService.deleteProjectById(id);
+    }
+
 }
